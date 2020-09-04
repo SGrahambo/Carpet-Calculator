e0dd1cb3580940d078bb05c08ff42458b8ad4924
SGrahambo <sgrahambo101@gmail.com>
2020-09-03 21:52:37 -0700

    updating java that don't work.

---
+++ Carpet-Calculator.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+
+public class RoomDimension{
+	
+	
+	int width, length, area;
+	Scanner readme = new Scanner(System.in);
+	
+	public void setwidth() {
+	
+		width = readme.nextInt();		
+		
+	}
+	
+	public void setlength() {
+		
+		length = readme.nextInt();	
+		
+	}
+	
+	publint int getwidth() {
+		
+		width = 10;
+		return width;
+		
+	}
+	
+	public int getlength() {
+		length = 10;
+		return length;
+		
+	}
+	
+	
+	
+
+	
+	
+	
+	
+	
+}	
+//	public static void main(String[] args) {
+//		
+//		Scanner readme = new Scanner(System.in);
+//		
+//		double feetwidth, inchwidth, lengthfeet, lengthinch, area;
+//		
+//		System.out.println("Please enter the width of the area to be carpeted in feet and inches");
+//		System.out.println("Press Enter after each input: ");
+//		
+//		feetwidth = readme.nextDouble();
+//		inchwidth = readme.nextDouble();
+//		
+//		System.out.println("Please enter the lenth of the area to be carpeted in feet and inches");
+//		System.out.println("Press Enter after each input: ");
+//		
+//		lengthfeet = readme.nextDouble();
+//		lengthinch = readme.nextDouble();
+		
+		
+		
+	
+	
+
+
+public class RoomCarpet
+{
+	
+	
+	
+	
+	
+}
+		
+	
+	public static void main(String[] args) {
+		
+		Scanner readme = new Scanner(System.in);
+		
+	
+									
+	}
+	
+	
+}
---
+++ Main.java
@@ -0,0 +1,18 @@
+
+//*	
+	//Name: Stephen Graham
+	//CptS 233: MicroAssignment #1
+	//Date: 8/29/20
+	//git rep url: https://github.com/SGrahambo/Carpet-Calculator.git
+	//*
+
+
+public class Main {
+	public static void main(String[] args) {
+		
+		 RoomDimenstion dim = new RoomDimension;
+		 getwidth = dim.getwidth;
+		 getwidth;
+		 System.out.println("The width is: " + getwidth;
+	}
+}
